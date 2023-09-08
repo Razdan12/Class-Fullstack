5060434e7dce50cf1b3c79f55dd4c5125901d127
Nur Cahyanto <nur.razdan@gmail.com>
2023-09-08 08:16:27 +0700

    basic javascript

--- home.js
+++ home.js
@@ -1,8 +1,82 @@
-const a = 1;
-const b = 2;
+//string
 
-const c = a + b;
+// const a = 'ini string';
 
-console.log(c);
-console.log(c);
-console.log(c);
+// console.log(a);
+
+//number
+
+// const a = 202.3;
+// console.log(a);
+
+//object
+
+// const a = {
+//     hewan: "kucing",
+//     benda: "meja",
+//     nama: "meja",
+//     angka: 1234,
+// };
+// console.log(a);
+
+// operator
+
+// const bil1 = 40;
+// const bil2 = 20;
+
+// let bil3 = bil1 - bil2;
+// console.log(bil3);
+
+// const bilangan = {
+//     bil1: 98,
+//     bil2: 20,
+//     bil3: 234,
+//     bil4: 42
+// }
+
+// const bil5 = bilangan.bil4 + bilangan.bil3
+// console.log(bil5);
+
+// const bilangan1 = {
+//   bil1: 4,
+//   bil2: 6,
+//   bil3: 9,
+// };
+// const bilangan2 = {
+//   bil1: 54,
+//   bil2: 34,
+//   bil3: 21,
+// };
+// const bilangan3 = {
+//   bil1: 34,
+//   bil2: 64,
+//   bil3: 91,
+// };
+
+// const hasil = bilangan1.bil1 + bilangan3.bil2
+// console.log(hasil);
+
+// if (bilangan1.bil2 < bilangan2.bil2) {
+  
+//     if (bilangan1.bil2 < bilangan2.bil2){
+//         console.log("ini benar");
+//     }
+//     console.log("ini sangat salah");
+    
+// } else if (bilangan3.bil1 === bilangan1.bil1) {
+//   console.log("salah");
+// } else {
+//   console.log("sangat salah");
+// }
+
+
+// const role = "user";
+// const b = 8;
+// const login = false;
+
+// const hasil = !login ? "berhasil login" : "gagal login";
+// console.log(hasil);
+
+for (let i = 0; i < 10; i--){
+    console.log('ini nilai i : ', i);
+}
